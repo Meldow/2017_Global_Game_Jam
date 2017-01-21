f2ce1d1c1a7b34cdd3dd6f24c2eb950d180a420f
hugodsc <hugo.sousa.cardoso@tecnico.ulisboa.pt>
2017-01-21 03:34:51 +0000

    Enemy spawner
    Enemy spawner with ui editor buttons

---
+++ Assets/EnemySpawner/Scripts/EnemyBasicBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spawner;
+
+namespace Spawner{
+	public class EnemyBasicBehaviour : MonoBehaviour {
+
+		public float speed;
+		private Vector3 target = new Vector3(0,1,0);
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards (transform.position, target, step);
+		}
+	}
+}
---
+++ Assets/EnemySpawner/Scripts/SpawnerManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spawner;
+
+namespace Spawner{
+	public class SpawnerManager : MonoBehaviour {
+		private  GameObject[] spawnPoints;
+		public GameObject redEnemy;
+		public GameObject greenEnemy;
+		public GameObject blueEnemy;
+		public GameObject yellowEnemy;
+		public enum ENEMY_COLORS {RED,GREEN,BLUE,YELLOW}; 
+		// Use this for initialization
+		void Start () {
+			if(spawnPoints==null)
+				spawnPoints = GameObject.FindGameObjectsWithTag ("Spawn");
+		}
+
+		public void spawnRedEnemy(){
+			GameObject spawnPoint = spawnPoints[(Random.Range(0,spawnPoints.Length))];
+			Instantiate (redEnemy, spawnPoint.transform.position, Quaternion.Euler(90,0,0));
+		}
+			
+		public void spawnBlueEnemy(){
+			GameObject spawnPoint = spawnPoints[(Random.Range(0,spawnPoints.Length))];
+			Instantiate (blueEnemy, spawnPoint.transform.position, Quaternion.Euler(90,0,0));
+		}
+			
+		public void spawnGreenEnemy(){
+			GameObject spawnPoint = spawnPoints[(Random.Range(0,spawnPoints.Length))];
+			Instantiate (greenEnemy, spawnPoint.transform.position, Quaternion.Euler(90,0,0));
+		}
+			
+		public void spawnYellowEnemy(){
+			GameObject spawnPoint = spawnPoints[(Random.Range(0,spawnPoints.Length))];
+			Instantiate (yellowEnemy, spawnPoint.transform.position, Quaternion.Euler(90,0,0));
+		}
+
+		public void spawnRandomColor(){
+			GameObject spawnPoint = spawnPoints[(Random.Range(0,spawnPoints.Length))];
+			int colorIndex = Random.Range (0, 3);
+			switch (colorIndex) {
+			case 0:
+				Instantiate (redEnemy, spawnPoint.transform.position, Quaternion.Euler(90,0,0));
+				break;
+			case 1:
+				Instantiate (greenEnemy, spawnPoint.transform.position, Quaternion.Euler(90,0,0));
+				break;
+			case 2:
+				Instantiate (blueEnemy, spawnPoint.transform.position, Quaternion.Euler(90,0,0));
+				break;
+			case 3:
+				Instantiate (yellowEnemy, spawnPoint.transform.position, Quaternion.Euler(90,0,0));
+				break;
+			}
+		}
+
+		public void spawnEnemiesWithColorAndNumber(ENEMY_COLORS color, int numberOfEnemies){
+			switch (color) {
+			case ENEMY_COLORS.BLUE:
+				GameObject spawnPoint = spawnPoints[(Random.Range(0,spawnPoints.Length))];
+				for (int i = 0; i < numberOfEnemies; i++) {
+					Vector3 newPosition = new Vector3(spawnPoint.transform.position.x + (i*2),spawnPoint.transform.position.y,spawnPoint.transform.position.z);
+					Instantiate (blueEnemy, newPosition, Quaternion.Euler(90,0,0));
+				}
+				break;
+			case ENEMY_COLORS.GREEN:
+				for (int i = 0; i < numberOfEnemies; i++) {
+					spawnGreenEnemy ();
+				}
+				break;
+
+			case ENEMY_COLORS.RED:
+				for (int i = 0; i < numberOfEnemies; i++) {
+					spawnRedEnemy ();
+				}
+				break;
+			case ENEMY_COLORS.YELLOW:
+				for (int i = 0; i < numberOfEnemies; i++) {
+					spawnYellowEnemy ();
+				}
+				break;
+			}
+		}
+	}
+}
