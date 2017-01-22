311c5bbd3fe8fdb12cbf64f8aa42d578e4542c66
Alexandre Araújo Costa <alexandremacc@gmail.com>
2017-01-22 02:26:29 +0000

    Initial towers

---
+++ Assets/Enemy/Towers/TowerManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using Util;
+
+namespace Enemy {
+    public class TowerManager : Singleton<TowerManager> {
+        [SerializeField]
+        private GameObject RedTowerSpawnpoint;
+        [SerializeField]
+        private GameObject RedTower;
+        [SerializeField]
+        private GameObject BlueTowerSpawnpoint;
+        [SerializeField]
+        private GameObject BlueTower;
+        [SerializeField]
+        private GameObject YellowTowerSpawnpoint;
+        [SerializeField]
+        private GameObject YellowTower;
+        [SerializeField]
+        private GameObject GreenTowerSpawnpoint;
+        [SerializeField]
+        private GameObject GreenTower;
+
+        public void SpawnRedTower() {
+            var tower = Instantiate(RedTower);
+            tower.transform.position = RedTowerSpawnpoint.transform.position;
+        }
+
+        public void SpawnBlueTower() {
+            var tower = Instantiate(BlueTower);
+            tower.transform.position = BlueTowerSpawnpoint.transform.position;
+        }
+
+        public void SpawnGreenTower() {
+            var tower = Instantiate(GreenTower);
+            tower.transform.position = GreenTowerSpawnpoint.transform.position;
+        }
+
+        public void SpawnYellowTower() {
+            var tower = Instantiate(YellowTower);
+            tower.transform.position = YellowTowerSpawnpoint.transform.position;
+        }
+
+    }
+}
--- Assets/Player/Attack.cs
+++ Assets/Player/Attack.cs
@@ -47,7 +47,6 @@ namespace Player {
             }
         }
 
-
         //Input
         [SerializeField]
         private float chargeSpeed;
